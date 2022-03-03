4b800bf20e0de24874cfc255e737ecacad4ab599
brunoserrate <brunostj@hotmail.com>
2022-03-02 21:18:28 -0300

    Add wbweb/wbcore

---
+++ wbweb/src/layouts/MainLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ wbweb/src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+<q-page class="q-ma-lg">
+  <div class="row gutter-lg">
+    <div class="col">
+      <img :src="img" width="80%" :alt="category">
+    </div>
+    <div class="col">
+      <q-btn color="primary" icon="refresh" label="resetar" @click="resetar" />
+    </div>
+  </div>
+</q-page>
+  <!-- <q-page class="flex flex-center">
+  </q-page> -->
+</template>
+
+<script>
+//https://api.waifu.pics/type/category
+const pathApi = 'https://api.waifu.pics/sfw/';
+
+export default {
+	// props:['breadcrumbs'],
+	data (){
+		return {
+      img: '',
+      category:'cringe',
+		}
+	},
+	mounted(){
+    this.resetar()
+    //https://api.waifu.pics/type/category
+	},
+	methods:{
+    resetar(){
+      let that = this
+
+      that.$axios.get(pathApi + `${that.category}`)
+      .then((res) => {
+        console.log(res)
+        that.img = res.data.url
+      })
+    }
+	}
+};
+</script>
