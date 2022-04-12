d89549a7d722d3889ae1bfbfc144d5b6850c2457
LaisRCroft <laisassisrequena@gmail.com>
2022-04-12 18:54:29 -0300

    inciar leitura categorias

--- wbweb/src/pages/iniciar_leitura.vue
+++ wbweb/src/pages/iniciar_leitura.vue
@@ -16,44 +16,88 @@
 				</div>
 			</div>
         </div>
-		<div class="row">
-            <div class="col-2" v-for="(livro, i) in livros.slice(0, 5)" :key="i" style="margin: 0px 0px 0 51px;">
+		<!-- <div class="row">
+            <div class="col-2" v-for="(livro, i) in livros.slice(0, 5)" :key="i" style="margin: 0px -34px 0px 51px;">
                 <q-card class="card_imagem">
-                    <h4 class="titulo_livro">{{livro.attributes.titulo}}</h4>
-					<hr />
 					<q-card>
-						<div class="img_cover">
-							<div class="absolute-full text-subtitle2 flex flex-center coverImage">
-								Capa
+						<div>
+							<div class="flex flex-center">
+								<img :src="livro.caminho_capa" class="cover_style"/>
 							</div>
 						</div>
 					</q-card>
-					<!-- <q-card class="cover">
-						{{livro.attributes.capa}}
-					</q-card> -->
-                    <p>{{livro.attributes.sinopse}} ...</p>
 
                 </q-card>
             </div>
+		</div> -->
+		<div class="row">
+			<div class="col">
+				<vueper-slides
+					class="no-shadow slides_style"
+					:visible-slides="5"
+					:arrows="true"
+					:slide-ratio="1 / 4"
+					 slide-multiple
+					:gap="3"
+					:bullets="false"
+					:dragging-distance="70">
+						<vueper-slide
+							v-for="(livro, i) in livros" :key="i"
+							:image="livro.caminho_capa"
+						/>
+				</vueper-slides>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-2">
+				
+			</div>
 		</div>
     </q-page>
 </template>
 <script>
-
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 export default {
 	// props:['breadcrumbs'],
 	data (){
 		return {
 			sessao: false,
-			livros:[]
+			livros:[],
+			slide: 1,
+			slides: [
+				{
+					title: 'Slide #1',
+					content: 'Slide content.'
+				}
+			]
+			
 		}
 	},
+	mounted(){
+		// this.getLivros()
+		this.buscarLivros()
+	},
+	components: { VueperSlides, VueperSlide },
 	methods:{
-		async getLivros(){
+		 /*async getLivros(){
 			this.livros = await this.$axios.get('livros');
 			this.livros = this.livros.data.data
 			console.log("livros: ", this.livros)
 			this.cutSinopse()
+		}, */
+		
+		buscarLivros(){
+			let that = this
+
+			that.$axios.get(that.$pathAPI + '/historia?limit=10')
+			.then((res) => {
+				that.livros = res.data.data
+				console.log(that.livros)
+			})
+			.catch((err) => {
+				console.log(err.response)
+			})
 		},
 		cutSinopse(){
 			for(let i=0; i < this.livros.length; i++){
@@ -61,9 +105,6 @@ export default {
 				// console.log(this.livros[i].attributes.sinopse)
 			}
 		},
-	},
-	mounted(){
-		this.getLivros()
 	},
 };
 </script>
