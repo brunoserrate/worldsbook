7d5e3fffba15a2f8612d536f8034b6179b1f8e26
LaisRCroft <laisassisrequena@gmail.com>
2022-05-18 06:11:43 -0300

    barra de pesquisa

--- wbweb/src/pages/Error404.vue
+++ wbweb/src/pages/Error404.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
-        404
+    <div class="fullscreen background-cover text-white text-center q-pa-md flex flex-center">
+      <div class="row">
+        <div class="col-5" style="">
+          <div class="row">
+            <div class="col-12 offset-2">
+              <h1 style="font-size: 80px; font-weight: bold; font-family: Raleway; margin: 144px 0px 0px 0; color: #000;">404 not found</h1>
+            </div>
+            <div class="col-12 offset-2">
+              <h4 style="margin-top: 0; margin-bottom: 0; color: #000; font-size: 25px;">A página que você deseja não foi encontrada</h4>
+            </div>
+            <div class="col-12 offset-2">
+              <q-btn
+                class="q-mt-xl botao"
+                unelevated
+                to="/"
+                label="Clique para voltar ao início"
+                no-caps
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-7">
+          <img src="~assets/mascote404.png" style="width: 39%; margin: 0 190px 0 0;"/>
+        </div>
       </div>
-
-      <div class="text-h2" style="opacity:.4">
-        Oops. Nothing here...
-      </div>
-
-      <q-btn
-        class="q-mt-xl"
-        color="white"
-        text-color="blue"
-        unelevated
-        to="/"
-        label="Go Home"
-        no-caps
-      />
     </div>
-  </div>
 </template>
 
 <script>
@@ -27,3 +32,27 @@ export default {
   name: 'Error404'
 }
 </script>
+<style lang="scss" scoped>
+  .background-cover {
+      background: url('~assets/background.svg');
+      background-size: 27%;
+      // position: absolute;
+      background-repeat: no-repeat;
+      background-position: right center;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      // z-index: 10000;
+  }
+
+  .botao {
+    margin: 51px 0 0 0;
+    color: #7a22a7;
+    text-transform: uppercase;
+    font-size: 17px;
+    &:hover {
+      background-color: #7b22a734;
+    }
+  }
+</style>
